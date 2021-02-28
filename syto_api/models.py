b67bd5112a677de98fda0513b8723368698d03a5
Przemek Kaliś <przemek.kalis@gmail.com>
2021-02-28 11:44:27 +0100

    enhance availability period model

--- syto_api/models.py
+++ syto_api/models.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.backends import get_user_model
 from django.db import models
+from django.db.models import ExpressionWrapper, F
 
 UserModel = get_user_model()
 
 
+class AvailabilityPeriodQuerySet(models.QuerySet):
+    def with_timedelta(self):
+        timedelta_expression = ExpressionWrapper(
+            F("end") - F("start"), output_field=models.DurationField()
+        )
+        return self.annotate(timedelta=timedelta_expression)
+
+
 class AvailabilityPeriod(models.Model):
+
+    objects = AvailabilityPeriodQuerySet.as_manager()
 
     start = models.DateTimeField()
     end = models.DateTimeField()
