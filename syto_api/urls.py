8858f098d95ad541721afb031c1d5ae559857fdd
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-01 16:02:33 +0100

    add user viewset

--- syto_api/urls.py
+++ syto_api/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 from .apps import SytoApiConfig
+from .routers import router
 
 app_name = SytoApiConfig.name
 urlpatterns = [
+    path("", include(router.urls)),
     path("availability", views.AvailabilityView.as_view(), name="availability-list"),
 ]
