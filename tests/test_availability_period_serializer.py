a28051520129ce464923032149c081444da21d68
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-01 10:48:03 +0100

    enhance validation rules

--- tests/test_availability_period_serializer.py
+++ tests/test_availability_period_serializer.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import date, timedelta
 
 import pytest
 from dateutil import tz
@@ -8,14 +8,17 @@ from django.utils import timezone
 from syto_api.serializers import AvailabilityPeriodSerializer
 
 tz = tz.gettz(settings.TIME_ZONE)
-CURRENT_TIME = timezone.datetime(2021, 1, 1, 6, 0, 0, tzinfo=tz)
+TODAY = date.today()
+START = timezone.datetime(TODAY.year, TODAY.month, TODAY.day, 6, 0, 0, tzinfo=tz)
 
 
 @pytest.mark.django_db
 def test_valid_data(syto_user):
+    start = START
+    end = START + timedelta(hours=8)
     data = {
-        "start": CURRENT_TIME,
-        "end": CURRENT_TIME + timedelta(hours=8),
+        "start": start,
+        "end": end,
         "user": syto_user("foo@bar.baz").id,
     }
 
@@ -25,13 +28,60 @@ def test_valid_data(syto_user):
 
 
 @pytest.mark.django_db
-def test_invalid_data(syto_user):
+def test_start_and_end_not_at_the_same_day(syto_user):
+    start = START + timedelta(hours=16)  # today at 22:00
+    end = start + timedelta(hours=8)  # tomorrow at 6:00
     data = {
-        "start": CURRENT_TIME,
-        "end": CURRENT_TIME + timedelta(hours=24),
+        "start": start,
+        "end": end,
         "user": syto_user("foo@bar.baz").id,
     }
 
     serializer = AvailabilityPeriodSerializer(data=data)
+    serializer.is_valid()
 
-    assert not serializer.is_valid()
+    assert len(serializer.errors) == 1
+    assert (
+        serializer.errors["non_field_errors"][0]
+        == "Start and end must be at the same day."
+    )
+
+
+@pytest.mark.django_db
+def test_exceeded_maximum_number_of_hours(syto_user):
+    start = START
+    end = start + timedelta(hours=17)
+    data = {
+        "start": start,
+        "end": end,
+        "user": syto_user("foo@bar.baz").id,
+    }
+
+    serializer = AvailabilityPeriodSerializer(data=data)
+    serializer.is_valid()
+
+    assert len(serializer.errors) == 1
+    assert (
+        serializer.errors["non_field_errors"][0]
+        == "Maximum allowed number of hours is 16."
+    )
+
+
+@pytest.mark.django_db
+def test_not_full_number_of_hours(syto_user):
+    start = START
+    end = start + timedelta(minutes=450)  # 7 hours 30 minutes
+    data = {
+        "start": start,
+        "end": end,
+        "user": syto_user("foo@bar.baz").id,
+    }
+
+    serializer = AvailabilityPeriodSerializer(data=data)
+    serializer.is_valid()
+
+    assert len(serializer.errors) == 1
+    assert (
+        serializer.errors["non_field_errors"][0]
+        == "Only full number of hours is allowed."
+    )
