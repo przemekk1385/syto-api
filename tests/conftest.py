197447ff8068769cc1f5e04e93f19a13ef1fb416
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-02 18:55:27 +0100

    add fixture for DRF

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,14 @@ from typing import Callable
 
 import pytest
 from django.contrib.auth.backends import get_user_model
+from rest_framework.test import APIClient
 
 UserModel = get_user_model()
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 
 @pytest.fixture
