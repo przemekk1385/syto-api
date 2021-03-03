6ccce7dd2bb1578ed5aadb5e6f6d34543f3c809a
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-03 08:45:43 +0100

    add Swagger

--- syto_api/urls.py
+++ syto_api/urls.py
@@ -1,15 +1,24 @@
+from pathlib import Path
+
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from . import views
 from .apps import SytoApiConfig
 from .routers import router
 
+API_VERSION_PREFIX = "v1"
+TEMPLATES_DIR = Path(SytoApiConfig.name)
+
 app_name = SytoApiConfig.name
 urlpatterns = [
-    path("", include(router.urls)),
+    path(f"{API_VERSION_PREFIX}/", include(router.urls)),
     path(
-        "availability/",
+        f"{API_VERSION_PREFIX}/availability/",
         views.total_availability_list_view,
         name="total-availability-list",
+    ),
+    path(
+        "swagger", TemplateView.as_view(template_name=TEMPLATES_DIR / "swagger-ui.html")
     ),
 ]
