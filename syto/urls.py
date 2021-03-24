ae320a90b9e8b798c26ba553e6be66654dabdf61
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-24 10:19:53 +0100

    add static stuff and admin app

--- syto/urls.py
+++ syto/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
 
 urlpatterns = [
+    path("", include("syto_panel.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("syto_api.urls")),
     path("api-token-auth/", views.obtain_auth_token),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
