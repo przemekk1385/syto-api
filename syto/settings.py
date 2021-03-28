d5c81ded643d5c7eea3e8b467a823f8f2947c602
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-29 00:10:34 +0200

    set date format

--- syto/settings.py
+++ syto/settings.py
@@ -113,6 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
+DATE_FORMAT = "Y-m-d"
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
